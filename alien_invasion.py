250ae49253c6b82eb0804b23d064aab3c74b316d
Antony Martinez <antonymartinez12@gmail.com>
2025-10-09 23:35:34 -0500

    Se crea clase principal del juego

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    def __init__(self):
+        
+        pygame.init()
+        pygame.display.set_caption("Alien Invasion")
+        
+        self.screen = pygame.display.set_mode((800, 600))
+        self.bg_color = (230, 230, 230)
+        self.clock = pygame.time.Clock()
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
